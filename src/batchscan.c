75b42a58909f563363dc417a3ce2ca4ebfcc251a
Bo Goeran Kvamme <bokvamme@users.sourceforge.net>
2006-06-04 13:03:33 +0000

    Some small cleanups in batchscan.c

--- src/batchscan.c
+++ src/batchscan.c
@@ -34,6 +34,8 @@
 #define MAXTAPS	8000
 #define FIELDS	15
 
+static const char tcbatchreportname[] =	"tcbatch.txt";
+
 /*
  * search for all tap files in directory 'rootdir', create a html report of all taps
  * found there (and optionally in any subdirectories).
@@ -50,8 +52,6 @@
  * On success, returns the number of files found.
  * On failure, returns -1.
  */
-
-static const char tcbatchreportname[] =	"tcbatch.txt";
 
 int batchscan(char *rootdir, int includesubdirs, int doscan)
 {
@@ -91,6 +91,7 @@ int batchscan(char *rootdir, int includesubdirs, int doscan)
 	ret = getcwd(fullpath, 256 - 2);
 	if (ret == NULL)
 		return -1;
+
 	fullpath[strlen(fullpath)] = SLASH;
 	fullpath[strlen(fullpath) + 1] = '\0';
  
@@ -135,226 +136,231 @@ int batchscan(char *rootdir, int includesubdirs, int doscan)
    
 	free_list(fl);
 
-	if (total_taps > 0) {
-		sprintf(lin, "\n  Found %d TAP files.", total_taps);
-		msgout(lin);
+	if (total_taps == 0) {
+		msgout("\nError: No taps were found.");
 
-		if (doscan) {
+		for (i = 0; i < MAXTAPS; i++)
+			free(taps[i]);
 
-			/* 
-			 * note: we use a temp file then rename it the the name of outfile
-			 * because the frontend cannot open a file that is in use.
-			 */
-			
-			chdir(exedir);
+		return total_taps;
+	}
 
-			deleteworkfiles();
+	sprintf(lin, "\n  Found %d TAP files.", total_taps);
+	msgout(lin);
 
-			/* create a new report file... */
+	if (doscan) {
 
-			fp = fopen(tcbatchreportname, "w+t");
-			if (fp == NULL)
-				return -1;
-
-			j = total_taps;	/* countdown for the taps remaining. */
-			failed_taps = 0;
-
-			batchmode = 1;
-			aborted = 0;
-        
-			/* Open each file in turn... */
-
-			for (i = 0; i < total_taps && !aborted; i++) {
-				chdir(fullpath);	/* switch to the taps directory */
-          
-				sprintf(lin, "\n\nTesting : %s",taps[i]->path);	/* display current tap name */
-				msgout(lin);
-				sprintf(lin, "\n%d remaining.", --j);	/* and amount remaining */
-				msgout(lin);
- 
-				/* note : tap.name is updated to be filename only. */
-
-				if (load_tap(taps[i]->path)) {
-					analyze();
-					report();
-
-					/* copy required info to current db slot... */
-
-					strcpy(taps[i]->path, tap.path);
-					strcpy(taps[i]->name, tap.name);
-					taps[i]->len = tap.len;
-					taps[i]->detected_percent = tap.detected_percent;
-					taps[i]->purity = tap.purity;
-					taps[i]->total_data_files = tap.total_data_files;
-					taps[i]->total_gaps = tap.total_gaps;
-					taps[i]->fdate = tap.fdate;
-					taps[i]->version = tap.version;
-					taps[i]->crc = tap.crc;
-					taps[i]->cbmcrc = tap.cbmcrc;
-					taps[i]->cbmid = tap.cbmid;
-					strcpy(taps[i]->cbmname, tap.cbmname);
-					taps[i]->tst_hd = tap.tst_hd;
-					taps[i]->tst_rc = tap.tst_rc;
-					taps[i]->tst_op = tap.tst_op;
-					taps[i]->tst_cs = tap.tst_cs;
-					taps[i]->tst_rd = tap.tst_rd;
-
-					if (tap.tst_hd || tap.tst_rc || tap.tst_op || tap.tst_cs || tap.tst_rd)
-						failed_taps++;
-				}
-			}
-
-			total_scanned = i;	/* user may have aborted hence this!.  */
-
-			/* sort taps[] by cbmcrc... */
-
-			if (sortbycrc) {
-				do {
-					swaps = 0;
-					for (i = 0; i < total_scanned - 1; i++) {
-						if (taps[i]->cbmcrc > taps[i + 1]->cbmcrc) {
-							tmp = taps[i];
-							taps[i] = taps[i + 1];
-							taps[i + 1] = tmp;
-							swaps++;
-						}
-					}
-				} while(swaps != 0);
-			} else {
-
-				/* sort taps[] by filename... */
-
-				do {
-					swaps = 0;
-					for (i = 0; i < total_scanned - 1; i++) {
-
-						/* sort by filename A-Z. */
-
-						if (strcmp(taps[i]->path, taps[i + 1]->path) > 0) {
-							tmp = taps[i];
-							taps[i] = taps[i + 1];
-							taps[i + 1] = tmp;
-							swaps++;
-						}
-					}
-				} while(swaps != 0);
-			}
-
-			/* prepare and print the report... */
-
-			fprintf(fp, "\nTAPClean Batch Report\n");
-
-			/* pad each field header to match its longest data item... */
-
-			for (i = 0; i < total_scanned; i++) {
-				sprintf(tfields[0], "%s", taps[i]->path);
-				sprintf(tfields[1], "%d%%", taps[i]->detected_percent);
-				sprintf(tfields[2], "%s", cstr[taps[i]->tst_rc]);
-				sprintf(tfields[3], "%s", cstr[taps[i]->tst_hd]);
-				sprintf(tfields[4], "%s", cstr[taps[i]->tst_op]);
-				sprintf(tfields[5], "%s", cstr[taps[i]->tst_cs]);
-				sprintf(tfields[6], "%s", cstr[taps[i]->tst_rd]);
-				sprintf(tfields[7], "%d", taps[i]->total_data_files);
-				sprintf(tfields[8], "%d", taps[i]->total_gaps);
-				sprintf(tfields[9], "%08X", taps[i]->crc);
-				sprintf(tfields[10], "%d", taps[i]->version);
-				sprintf(tfields[11], "%d", taps[i]->purity);
-				sprintf(tfields[12], "%d", taps[i]->len);
-				sprintf(tfields[13], "%08X", taps[i]->cbmcrc);
-				sprintf(tfields[14], "%s", knam[taps[i]->cbmid]);
-
-				for (j =0 ; j < FIELDS; j++) {	/* add padding if necessary... */
-					if (strlen(tfields[j]) > strlen(fields[j]))
-						padstring(fields[j], strlen(tfields[j]));
-				}
-			}
-
-			/* print field headers to file... */
-
-			fprintf(fp, "\n");
-			for (i = 0; i < FIELDS; i++)
-				fprintf(fp, "%s  ",fields[i]);
-			fprintf(fp, "\n");
-
-			/* print data fields... */
-
-			for (i = 0; i < total_scanned; i++) {
-				sprintf(tfields[0], "%s", taps[i]->path);
-				sprintf(tfields[1], "%d%%", taps[i]->detected_percent);
-				sprintf(tfields[2], "%s", cstr[taps[i]->tst_rc]);
-				sprintf(tfields[3], "%s", cstr[taps[i]->tst_hd]);
-				sprintf(tfields[4], "%s", cstr[taps[i]->tst_op]);
-				sprintf(tfields[5], "%s", cstr[taps[i]->tst_cs]);
-				sprintf(tfields[6], "%s", cstr[taps[i]->tst_rd]);
-				sprintf(tfields[7], "%d", taps[i]->total_data_files);
-				sprintf(tfields[8], "%d", taps[i]->total_gaps);
-				sprintf(tfields[9], "%08X", taps[i]->crc);
-				sprintf(tfields[10], "%d", taps[i]->version);
-				sprintf(tfields[11], "%d", taps[i]->purity);
-				sprintf(tfields[12], "%d", taps[i]->len);
-				sprintf(tfields[13], "%08X", taps[i]->cbmcrc);
-				sprintf(tfields[14], "%s", knam[taps[i]->cbmid]);
-
-				/* pad each data field to match its field header if necessary... */
-
-				for (j = 0 ; j < FIELDS; j++) {
-					if (strlen(tfields[j]) < strlen(fields[j]))
-						padstring(tfields[j], strlen(fields[j]));
-				}
-
-				/* print data fields to file... */
-
-				fprintf(fp,"\n");
-				for (j = 0; j < FIELDS; j++)
-					fprintf(fp, "%s  ", tfields[j]);
-			}
-         
-			fprintf(fp, "\n");
-			fprintf(fp, "\nTotal TAPs : %d ", i);
-			fprintf(fp, "\nFailed : %d ", failed_taps);
-
-			/* print key to table headers... */
+		/* 
+		 * note: we use a temp file then rename it the the name of outfile
+		 * because the frontend cannot open a file that is in use.
+		 */
 		
-			fprintf(fp, "\n\n");
-			fprintf(fp, "\nName     - The name of the tap file.");
-			fprintf(fp, "\nDetected - The percentage of the tap file that contains known files.");
-			fprintf(fp, "\nRec      - Recognized : Test result for capacity of the tap filled with known files (see 'Detected').");
-			fprintf(fp, "\nHdr      - Header : Test result for the tap file header.");
-			fprintf(fp, "\nOpt      - Optimized : Test result for the optimization status of known files stored in the tap.");
-			fprintf(fp, "\nChks     - Checksums : Test result for all checksums found in known files stored in the tap.");
-			fprintf(fp, "\nRead     - Read Errors : Test result for read errors present in known files stored in the tap.");
-			fprintf(fp, "\nFiles    - The total number of valid files found in the tap.");
-			fprintf(fp, "\nGaps     - The total number of gaps found in the tap.");
-			fprintf(fp, "\nCRC32    - The overall CRC32 checksum of all files stored in the tap.");
-			fprintf(fp, "\nVer      - Version : The tap file version.");
-			fprintf(fp, "\nVar      - Variations : The number of unique byte values present in the data section of the tap file.");
-			fprintf(fp, "\nSize     - The size of the tap file in bytes.");
-			fprintf(fp, "\ncbmCRC32 - The CRC32 checksum of the first CBM DATA file (if any) found in the tap.");
-			fprintf(fp, "\nLoaderID - The name of the loader program contained in the first CBM DATA file (if exists\\is known).");
-			fprintf(fp, "\n\n");
-			fprintf(fp, "\nGenerated by %s", VERSION_STR);
-			fprintf(fp, "\n\n");
-			fclose(fp);
+		chdir(exedir);
 
-			chdir(exedir);
-           
-			/*  print path/name of batch report to screen.. */
+		deleteworkfiles();
 
-			sprintf(temp, fullpath);
-			strcat(temp, tcbatchreportname);
-			sprintf(lin, "\n\nSaved : %s", temp);
+		/* create a new report file... */
+
+		fp = fopen(tcbatchreportname, "w+t");
+		if (fp == NULL)
+			return -1;
+
+		j = total_taps;	/* countdown for the taps remaining. */
+		failed_taps = 0;
+
+		batchmode = 1;
+		aborted = 0;
+        
+		/* Open each file in turn... */
+
+		for (i = 0; i < total_taps && !aborted; i++) {
+			chdir(fullpath);	/* switch to the taps directory */
+         
+			sprintf(lin, "\n\nTesting : %s",taps[i]->path);	/* display current tap name */
 			msgout(lin);
+			sprintf(lin, "\n%d remaining.", --j);	/* and amount remaining */
+			msgout(lin);
+ 
+			/* note : tap.name is updated to be filename only. */
 
-			aborted = 0;
+			if (load_tap(taps[i]->path)) {
+				analyze();
+				report();
+
+				/* copy required info to current db slot... */
+
+				strcpy(taps[i]->path, tap.path);
+				strcpy(taps[i]->name, tap.name);
+				taps[i]->len = tap.len;
+				taps[i]->detected_percent = tap.detected_percent;
+				taps[i]->purity = tap.purity;
+				taps[i]->total_data_files = tap.total_data_files;
+				taps[i]->total_gaps = tap.total_gaps;
+				taps[i]->fdate = tap.fdate;
+				taps[i]->version = tap.version;
+				taps[i]->crc = tap.crc;
+				taps[i]->cbmcrc = tap.cbmcrc;
+				taps[i]->cbmid = tap.cbmid;
+				strcpy(taps[i]->cbmname, tap.cbmname);
+				taps[i]->tst_hd = tap.tst_hd;
+				taps[i]->tst_rc = tap.tst_rc;
+				taps[i]->tst_op = tap.tst_op;
+				taps[i]->tst_cs = tap.tst_cs;
+				taps[i]->tst_rd = tap.tst_rd;
+
+				if (tap.tst_hd || tap.tst_rc || tap.tst_op || tap.tst_cs || tap.tst_rd)
+					failed_taps++;
+			}
 		}
 
-		time(&t2);
-		time2str(t2 - t1, lin);
-		printf("\n\nOperation completed in %s.", lin);
+		total_scanned = i;	/* user may have aborted hence this!.  */
 
-	} else
-		msgout("\nError: No taps were found.");
-   
+		/* sort taps[] by cbmcrc... */
+
+		if (sortbycrc) {
+			do {
+				swaps = 0;
+				for (i = 0; i < total_scanned - 1; i++) {
+					if (taps[i]->cbmcrc > taps[i + 1]->cbmcrc) {
+						tmp = taps[i];
+						taps[i] = taps[i + 1];
+						taps[i + 1] = tmp;
+						swaps++;
+					}
+				}
+			} while(swaps != 0);
+		} else {
+
+			/* sort taps[] by filename... */
+
+			do {
+				swaps = 0;
+				for (i = 0; i < total_scanned - 1; i++) {
+
+					/* sort by filename A-Z. */
+
+					if (strcmp(taps[i]->path, taps[i + 1]->path) > 0) {
+						tmp = taps[i];
+						taps[i] = taps[i + 1];
+						taps[i + 1] = tmp;
+						swaps++;
+					}
+				}
+			} while(swaps != 0);
+		}
+
+		/* prepare and print the report... */
+
+		fprintf(fp, "\nTAPClean Batch Report\n");
+
+		/* pad each field header to match its longest data item... */
+
+		for (i = 0; i < total_scanned; i++) {
+			sprintf(tfields[0], "%s", taps[i]->path);
+			sprintf(tfields[1], "%d%%", taps[i]->detected_percent);
+			sprintf(tfields[2], "%s", cstr[taps[i]->tst_rc]);
+			sprintf(tfields[3], "%s", cstr[taps[i]->tst_hd]);
+			sprintf(tfields[4], "%s", cstr[taps[i]->tst_op]);
+			sprintf(tfields[5], "%s", cstr[taps[i]->tst_cs]);
+			sprintf(tfields[6], "%s", cstr[taps[i]->tst_rd]);
+			sprintf(tfields[7], "%d", taps[i]->total_data_files);
+			sprintf(tfields[8], "%d", taps[i]->total_gaps);
+			sprintf(tfields[9], "%08X", taps[i]->crc);
+			sprintf(tfields[10], "%d", taps[i]->version);
+			sprintf(tfields[11], "%d", taps[i]->purity);
+			sprintf(tfields[12], "%d", taps[i]->len);
+			sprintf(tfields[13], "%08X", taps[i]->cbmcrc);
+			sprintf(tfields[14], "%s", knam[taps[i]->cbmid]);
+
+			for (j =0 ; j < FIELDS; j++) {	/* add padding if necessary... */
+				if (strlen(tfields[j]) > strlen(fields[j]))
+					padstring(fields[j], strlen(tfields[j]));
+			}
+		}
+
+		/* print field headers to file... */
+
+		fprintf(fp, "\n");
+		for (i = 0; i < FIELDS; i++)
+			fprintf(fp, "%s  ",fields[i]);
+		fprintf(fp, "\n");
+
+		/* print data fields... */
+
+		for (i = 0; i < total_scanned; i++) {
+			sprintf(tfields[0], "%s", taps[i]->path);
+			sprintf(tfields[1], "%d%%", taps[i]->detected_percent);
+			sprintf(tfields[2], "%s", cstr[taps[i]->tst_rc]);
+			sprintf(tfields[3], "%s", cstr[taps[i]->tst_hd]);
+			sprintf(tfields[4], "%s", cstr[taps[i]->tst_op]);
+			sprintf(tfields[5], "%s", cstr[taps[i]->tst_cs]);
+			sprintf(tfields[6], "%s", cstr[taps[i]->tst_rd]);
+			sprintf(tfields[7], "%d", taps[i]->total_data_files);
+			sprintf(tfields[8], "%d", taps[i]->total_gaps);
+			sprintf(tfields[9], "%08X", taps[i]->crc);
+			sprintf(tfields[10], "%d", taps[i]->version);
+			sprintf(tfields[11], "%d", taps[i]->purity);
+			sprintf(tfields[12], "%d", taps[i]->len);
+			sprintf(tfields[13], "%08X", taps[i]->cbmcrc);
+			sprintf(tfields[14], "%s", knam[taps[i]->cbmid]);
+
+			/* pad each data field to match its field header if necessary... */
+
+			for (j = 0 ; j < FIELDS; j++) {
+				if (strlen(tfields[j]) < strlen(fields[j]))
+					padstring(tfields[j], strlen(fields[j]));
+			}
+
+			/* print data fields to file... */
+
+			fprintf(fp,"\n");
+			for (j = 0; j < FIELDS; j++)
+				fprintf(fp, "%s  ", tfields[j]);
+		}
+         
+		fprintf(fp, "\n");
+		fprintf(fp, "\nTotal TAPs : %d ", i);
+		fprintf(fp, "\nFailed : %d ", failed_taps);
+
+		/* print key to table headers... */
+
+		fprintf(fp, "\n\n");
+		fprintf(fp, "\nName     - The name of the tap file.");
+		fprintf(fp, "\nDetected - The percentage of the tap file that contains known files.");
+		fprintf(fp, "\nRec      - Recognized : Test result for capacity of the tap filled with known files (see 'Detected').");
+		fprintf(fp, "\nHdr      - Header : Test result for the tap file header.");
+		fprintf(fp, "\nOpt      - Optimized : Test result for the optimization status of known files stored in the tap.");
+		fprintf(fp, "\nChks     - Checksums : Test result for all checksums found in known files stored in the tap.");
+		fprintf(fp, "\nRead     - Read Errors : Test result for read errors present in known files stored in the tap.");
+		fprintf(fp, "\nFiles    - The total number of valid files found in the tap.");
+		fprintf(fp, "\nGaps     - The total number of gaps found in the tap.");
+		fprintf(fp, "\nCRC32    - The overall CRC32 checksum of all files stored in the tap.");
+		fprintf(fp, "\nVer      - Version : The tap file version.");
+		fprintf(fp, "\nVar      - Variations : The number of unique byte values present in the data section of the tap file.");
+		fprintf(fp, "\nSize     - The size of the tap file in bytes.");
+		fprintf(fp, "\ncbmCRC32 - The CRC32 checksum of the first CBM DATA file (if any) found in the tap.");
+		fprintf(fp, "\nLoaderID - The name of the loader program contained in the first CBM DATA file (if exists\\is known).");
+		fprintf(fp, "\n\n");
+		fprintf(fp, "\nGenerated by %s", VERSION_STR);
+		fprintf(fp, "\n\n");
+		fclose(fp);
+
+		chdir(exedir);
+           
+		/*  print path/name of batch report to screen.. */
+
+		sprintf(temp, fullpath);
+		strcat(temp, tcbatchreportname);
+		sprintf(lin, "\n\nSaved : %s", temp);
+		msgout(lin);
+
+		aborted = 0;
+	}
+
+	time(&t2);
+	time2str(t2 - t1, lin);
+	printf("\n\nOperation completed in %s.", lin);
+
 	/* cleanup... */
 
 	for (i = 0; i < MAXTAPS; i++)
