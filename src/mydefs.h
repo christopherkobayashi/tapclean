0fc32292324ee7bd3c598a1dd2e0a69df32d5297
Luigi Di Fraia <luigidifraia@users.sourceforge.net>
2010-02-01 21:30:40 +0000

    Updated copyright dates

--- src/mydefs.h
+++ src/mydefs.h
@@ -55,8 +55,8 @@
 #define SLASH   '/'
 #endif
 
-#define VERSION_STR "TAPClean v0.23 Console - (C) 2006-2009 TC Team"
-#define BUILDER     "bgk"
+#define VERSION_STR "TAPClean v0.23 Console - (C) 2006-2010 TC Team"
+#define BUILDER     "Luigi Di Fraia"
 
 #define TRUE	1
 #define FALSE	0
