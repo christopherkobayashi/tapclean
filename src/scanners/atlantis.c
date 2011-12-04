8ab510abdcbfaed362a4f97632ca65797dc87b18
Luigi Di Fraia <luigidifraia@users.sourceforge.net>
2011-12-04 22:11:52 +0000

    Rewritten according to the new templates

--- src/scanners/atlantis.c
+++ src/scanners/atlantis.c
@@ -1,128 +1,267 @@
-/*---------------------------------------------------------------------------
-  atlantis.c (Atlantis Loader)
-  
-  Part of project "Final TAP". 
-  
-  A Commodore 64 tape remastering and data extraction utility.
+/*
+ * atlantis.c (rewritten by Luigi Di Fraia, Dec 2011 - armaeth@libero.it)
+ *
+ * Part of project "TAPClean". May be used in conjunction with "Final TAP".
+ *
+ * Final TAP is (C) 2001-2006 Stewart Wilson, Subchrist Software.
+ *
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+ * St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
 
-  (C) 2001-2006 Stewart Wilson, Subchrist Software.
-   
-  
-   
-   This program is free software; you can redistribute it and/or modify it under 
-   the terms of the GNU General Public License as published by the Free Software 
-   Foundation; either version 2 of the License, or (at your option) any later 
-   version.
-   
-   This program is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-   PARTICULAR PURPOSE. See the GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License along with 
-   this program; if not, write to the Free Software Foundation, Inc., 51 Franklin 
-   St, Fifth Floor, Boston, MA 02110-1301 USA
-   
----------------------------------------------------------------------------*/
+/*
+ * Status: Beta
+ *
+ * CBM inspection needed: No
+ * Single on tape: No
+ * Sync: Sequence (bytes)
+ * Header: Yes
+ * Data: Continuous
+ * Checksum: Yes
+ * Post-data: Yes
+ * Trailer: Yes
+ * Trailer homogeneous: Yes (bit 0 pulses)
+ */
 
 #include "../mydefs.h"
 #include "../main.h"
 
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#define HDSZ 6
+#define THISLOADER	ATLAN
 
-/*---------------------------------------------------------------------------*/
-void atlantis_search(void)
+#define BITSINABYTE	8	/* a byte is made up of 8 bits here */
+
+#define SYNCSEQSIZE	2	/* amount of sync bytes */
+#define MAXTRAILER	8	/* max amount of trailer pulses read in */
+
+#define HEADERSIZE	6	/* size of block header */
+
+#define LOADOFFSETH	4	/* load location (MSB) offset inside header */
+#define LOADOFFSETL	5	/* load location (LSB) offset inside header */
+#define ENDOFFSETH	2	/* end location (MSB) offset inside header */
+#define ENDOFFSETL	3	/* end location (LSB) offset inside header */
+#define EXECOFFSETH	0	/* start location (MSB) offset inside header */
+#define EXECOFFSETL	1	/* start location (LSB) offset inside header */
+
+void atlantis_search (void)
 {
-   int i,sof,sod,eod,eof;
-   int z,h,hd[HDSZ];
-   unsigned int s,e,x;
+	int i, h;			/* counters */
+	int sof, sod, eod, eof, eop;	/* file offsets */
+	int pat[SYNCSEQSIZE];		/* buffer to store a sync train */
+	int hd[HEADERSIZE];		/* buffer to store block header info */
 
-   if(!quiet)
-      msgout("  Atlantis Tape");
+	int en, tp, sp, lp, sv;		/* encoding parameters */
 
-   for(i=20; i<tap.len-8; i++)
-   {
-      if((z=find_pilot(i,ATLAN))>0)
-      {
-         sof=i;
-         i=z;
-         if(readttbyte(i, ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en)==ft[ATLAN].sv)
-         {
-            sod=i+8 +8;  /* extra +8 skips sync2 ($42)  */
-            /* decode the header so we can validate the addresses...  */
-            for(h=0; h<HDSZ; h++)
-            {
-               hd[h] = readttbyte(sod+(h*8), ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en);
-               if (hd[h] == -1)
-                  break;
-            }
-            if (h != HDSZ)
-               continue;
+	unsigned int s, e;		/* block locations referred to C64 memory */
+	unsigned int x; 		/* block size */
 
-            s=hd[5]+(hd[4]<<8);   /* get start address */
-            e=hd[3]+(hd[2]<<8);   /* get end address   */
-            if(e>s)
-            {
-               x=e-s;
-               eod=sod+((x+HDSZ)*8) +8;    /* note +8 points eod at trailing zero. */
-               eof=eod+7;
-               addblockdef(ATLAN, sof,sod,eod,eof, 0);
-               i=eof;  /* optimize search */
-            }
-         }
-      }
-      else
-      {
-         if(z<0)    /* find_pilot() failed (too few/many), set i to failure point. */
-            i=(-z);
-      }
-   }
+	/* Expected sync pattern */
+	static int sypat[SYNCSEQSIZE] = {
+		0x52, 0x42
+	};
+	int match;			/* condition variable */
+
+
+	en = ft[THISLOADER].en;
+	tp = ft[THISLOADER].tp;
+	sp = ft[THISLOADER].sp;
+	lp = ft[THISLOADER].lp;
+	sv = ft[THISLOADER].sv;
+
+	if (!quiet)
+		msgout("  Atlantis Tape");
+
+	for (i = 20; i > 0 && i < tap.len - BITSINABYTE; i++) {
+		eop = find_pilot(i, THISLOADER);
+
+		if (eop > 0) {
+			/* Valid pilot found, mark start of file */
+			sof = i;
+			i = eop;
+
+			/* Decode a 2 byte sequence (possibly a valid sync train) */
+			for (h = 0; h < SYNCSEQSIZE; h++)
+				pat[h] = readttbyte(i + (h * BITSINABYTE), lp, sp, tp, en);
+
+			/* Note: no need to check if readttbyte is returning -1, for
+			         the following comparison (DONE ON ALL READ BYTES)
+			         will fail all the same in that case */
+
+			/* Check sync train. We may use the find_seq() facility too */
+			for (match = 1, h = 0; h < SYNCSEQSIZE; h++)
+				if (pat[h] != sypat[h])
+					match = 0;
+
+			/* Sync train doesn't match */
+			if (!match)
+				continue;
+
+			/* Valid sync train found, mark start of data */
+			sod = i + BITSINABYTE * SYNCSEQSIZE;
+
+			/* Read header */
+			for (h = 0; h < HEADERSIZE; h++) {
+				hd[h] = readttbyte(sod + h * BITSINABYTE, lp, sp, tp, en);
+				if (hd[h] == -1)
+					break;
+			}
+			if (h != HEADERSIZE)
+				continue;
+
+			/* Extract load and end locations */
+			s = hd[LOADOFFSETL] + (hd[LOADOFFSETH] << 8);
+			e = hd[ENDOFFSETL]  + (hd[ENDOFFSETH]  << 8);
+
+			/* Prevent int wraparound when subtracting 1 from end location
+			   to get the location of the last loaded byte */
+			if (e == 0)
+				e = 0xFFFF;
+			else
+				e--;
+
+			/* Plausibility check */
+			if (e < s)
+				continue;
+
+			/* Compute size */
+			x = e - s + 1;
+
+			/* Point to the first pulse of the post-data byte (that's final) */
+			eod = sod + (HEADERSIZE + x + 1) * BITSINABYTE;
+
+			/* Initially point to the last pulse of the post-data byte */
+			eof = eod + BITSINABYTE - 1;
+
+			/* Trace 'eof' to end of trailer (bit 0 pulses only) */
+			h = 0;
+			while (eof < tap.len - 1 &&
+					h++ < MAXTRAILER &&
+					readttbit(eof + 1, lp, sp, tp) == 0)
+				eof++;
+
+			if (addblockdef(THISLOADER, sof, sod, eod, eof, 0) >= 0)
+				i = eof;	/* Search for further files starting from the end of this one */
+
+		} else {
+			if (eop < 0)
+				i = (-eop);
+		}
+	}
 }
-/*---------------------------------------------------------------------------*/
-int atlantis_describe(int row)
+
+int atlantis_describe (int row)
 {
-   int i,s,b,hd[HDSZ],rd_err,cb;
+	int i, s;
+	int hd[HEADERSIZE];
+	unsigned int exec;
+	int en, tp, sp, lp;
+	int cb;
 
-   /* decode header... */
-   s= blk[row]->p2;
-   for(i=0; i<HDSZ; i++)
-      hd[i]= readttbyte(s+(i*8), ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en);
-   
-   blk[row]->cs = hd[5]+(hd[4]<<8);
-   blk[row]->ce = hd[3]+(hd[2]<<8)-1;
-   blk[row]->cx = (blk[row]->ce - blk[row]->cs)+1;  
+	int b, rd_err;
 
-   /* get pilot trailer lengths   */
-   blk[row]->pilot_len = (blk[row]->p2 - blk[row]->p1 -8)>>3;
-   blk[row]->trail_len = (blk[row]->p4 - blk[row]->p3 -7)>>3;
 
-   /* extract data and test checksum... */
-   rd_err=0;
-   cb=0;
-   s= blk[row]->p2+(HDSZ*8);
+	en = ft[THISLOADER].en;
+	tp = ft[THISLOADER].tp;
+	sp = ft[THISLOADER].sp;
+	lp = ft[THISLOADER].lp;
 
-   if(blk[row]->dd!=NULL)
-      free(blk[row]->dd);
-   blk[row]->dd = (unsigned char*)malloc(blk[row]->cx);
+	/* Note: addblockdef() is the glue between ft[] and blk[], so we can now read from blk[] */
+	s = blk[row] -> p2;
 
-   for(i=0; i<blk[row]->cx; i++)
-   {
-      b= readttbyte(s+(i*8), ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en);
-      cb^=b;
-      if(b==-1)
-         rd_err++;
-      blk[row]->dd[i]=b;
-   }
-   b= readttbyte(s+(i*8), ft[ATLAN].lp, ft[ATLAN].sp, ft[ATLAN].tp, ft[ATLAN].en); /* read actual checkbyte. */
+	/* Read header (it's safe to read it here for it was already decoded during the search stage) */
+	for (i = 0; i < HEADERSIZE; i++)
+		hd[i]= readttbyte(s + i * BITSINABYTE, lp, sp, tp, en);
 
-   blk[row]->cs_exp= cb &0xFF;
-   blk[row]->cs_act= b;
-   blk[row]->rd_err= rd_err;
+	/* Extract load and end locations */
+	blk[row]->cs = hd[LOADOFFSETL] + (hd[LOADOFFSETH] << 8);
+	blk[row]->ce = hd[ENDOFFSETL]  + (hd[ENDOFFSETH]  << 8);
 
-   return 0;
+	/* Prevent int wraparound when subtracting 1 from end location
+	   to get the location of the last loaded byte */
+	if (blk[row]->ce == 0)
+		blk[row]->ce = 0xFFFF;
+	else
+		(blk[row]->ce)--;
+
+	/* Compute size */
+	blk[row]->cx = blk[row]->ce - blk[row]->cs + 1;
+
+	/* Compute pilot & trailer lengths */
+
+	/* pilot is in bytes... */
+	blk[row]->pilot_len = (blk[row]->p2 - blk[row]->p1) / BITSINABYTE;
+
+	/* ... trailer in pulses */
+	blk[row]->trail_len = blk[row]->p4 - blk[row]->p3 - (BITSINABYTE - 1);
+
+	/* if there IS pilot then disclude the sync sequence */
+	if (blk[row]->pilot_len > 0)
+		blk[row]->pilot_len -= SYNCSEQSIZE;
+
+	/* Extract data and test checksum... */
+	rd_err = 0;
+	cb = 0;
+
+	s = blk[row]->p2 + (HEADERSIZE * BITSINABYTE);
+
+	if (blk[row]->dd != NULL)
+		free(blk[row]->dd);
+
+	blk[row]->dd = (unsigned char*)malloc(blk[row]->cx);
+
+	for (i = 0; i < blk[row]->cx; i++) {
+		b = readttbyte(s + (i * BITSINABYTE), lp, sp, tp, en);
+		cb ^= b;
+
+		if (b != -1) {
+			blk[row]->dd[i] = b;
+		} else {
+			blk[row]->dd[i] = 0x69;  /* error code */
+			rd_err++;
+
+			/* for experts only */
+			sprintf(lin, "\n - Read Error on byte @$%X (prg data offset: $%04X)", s + (i * BITSINABYTE), i);
+			strcat(info, lin);
+		}
+	}
+
+	b = readttbyte(s + (i * BITSINABYTE), lp, sp, tp, en);
+	if (b == -1) {
+		/* Even if not within data, we cannot validate data reliably if
+		   checksum is unreadable, so that increase read errors */
+		rd_err++;
+
+		/* for experts only */
+		sprintf(lin, "\n - Read Error on checkbyte @$%X", s + (i * BITSINABYTE));
+		strcat(info, lin);
+	}
+
+	exec = hd[EXECOFFSETL] + (hd[EXECOFFSETH] << 8);
+	/* $036F is default and means return from custom Load RAM, so it's of no interest */
+	if (exec != 0x036F) {
+		sprintf(lin, "\n - Execution address: $%04X (SYS %d)", exec, exec);
+		strcat(info, lin);
+	}
+
+	blk[row]->cs_exp = cb & 0xFF;
+	blk[row]->cs_act = b  & 0xFF;
+	blk[row]->rd_err = rd_err;
+
+	return(rd_err);
 }
-
-
-
- 
