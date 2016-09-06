ab648cb51afa9e8822a9acb4b6800616c62c70f5
Luigi Di Fraia <luigidifraia@users.sourceforge.net>
2016-09-06 09:09:28 +0000

    Initial revision

---
+++ src/scanners/paldevel.c
@@ -0,0 +1,221 @@
+/*
+ * paldevel.c (by Luigi Di Fraia, Sep 2016 - armaeth@libero.it)
+ *
+ * Part of project "TAPClean". May be used in conjunction with "Final TAP".
+ *
+ * Based on ashdave.c.
+ * Final TAP is (C) 2001-2006 Stewart Wilson, Subchrist Software.
+ *
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+ * St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+/*
+ * Status: Beta
+ *
+ * CBM inspection needed: No
+ * Single on tape: No
+ * Sync: Byte
+ * Header: Yes
+ * Data: Continuous
+ * Checksum: No
+ * Post-data: Yes
+ * Trailer: Yes
+ * Trailer homogeneous: No
+ */
+
+#include "../mydefs.h"
+#include "../main.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define THISLOADER	PAL_DEVELOPMENTS
+
+#define BITSINABYTE	8	/* a byte is made up of 8 bits here */
+
+#define SYNCSEQSIZE	1	/* amount of sync bytes */
+#define MAXTRAILER	256	/* max amount of trailer pulses read in */
+
+#define HEADERSIZE	4	/* size of block header */
+
+#define LOADOFFSETH	1	/* load location (MSB) offset inside header */
+#define LOADOFFSETL	0	/* load location (LSB) offset inside header */
+#define ENDOFFSETH	3	/* end  location (MSB) offset inside header */
+#define ENDOFFSETL	2	/* end  location (LSB) offset inside header */
+
+void paldevel_search (void)
+{
+	int i, h;			/* counters */
+	int sof, sod, eod, eof, eop;	/* file offsets */
+	int hd[HEADERSIZE];		/* buffer to store block header info */
+
+	int en, tp, sp, lp, sv;		/* encoding parameters */
+
+	unsigned int s, e;		/* block locations referred to C64 memory */
+	unsigned int x; 		/* block size */
+
+
+	en = ft[THISLOADER].en;
+	tp = ft[THISLOADER].tp;
+	sp = ft[THISLOADER].sp;
+	lp = ft[THISLOADER].lp;
+	sv = ft[THISLOADER].sv;
+
+	if (!quiet)
+		msgout("  PAL Developments Tape");
+
+	for (i = 20; i > 0 && i < tap.len - BITSINABYTE; i++) {
+		eop = find_pilot(i, THISLOADER);
+
+		if (eop > 0) {
+			/* Valid pilot found, mark start of file */
+			sof = i;
+			i = eop;
+
+			/* Check if there's a valid sync byte for this loader */
+			if (readttbyte(i, lp, sp, tp, en) != sv)
+				continue;
+
+			/* Valid sync found, mark start of data */
+			sod = i + BITSINABYTE * SYNCSEQSIZE;
+
+			/* Read header */
+			for (h = 0; h < HEADERSIZE; h++) {
+				hd[h] = readttbyte(sod + h * BITSINABYTE, lp, sp, tp, en);
+				if (hd[h] == -1)
+					break;
+			}
+			if (h != HEADERSIZE)
+				continue;
+
+			/* Extract load and end locations */
+			s = hd[LOADOFFSETL] + (hd[LOADOFFSETH] << 8);
+			e = hd[ENDOFFSETL]  + (hd[ENDOFFSETH]  << 8);
+
+			/* Prevent int wraparound when subtracting 1 from end location
+			   to get the location of the last loaded byte */
+			if (e == 0)
+				e = 0xFFFF;
+			else
+				e--;
+
+			/* Plausibility check */
+			if (e < s)
+				continue;
+
+			/* Compute size */
+			x = e - s + 1;
+
+			/* Point to the first pulse of the last data byte (that's final) */
+			eod = sod + (HEADERSIZE + x - 1) * BITSINABYTE;
+
+			/* Point to the first pulse of the first postdata byte */
+			eof = eod + BITSINABYTE;
+
+			/* Trace 'eof' to end of trailer (any value, both bit 1 and bit 0 pulses) */
+			h = 0;
+			while (eof < tap.len - 1 &&
+					h++ < MAXTRAILER &&
+					readttbit(eof + 1, lp, sp, tp) >= 0)
+				eof++;
+
+			if (addblockdef(THISLOADER, sof, sod, eod, eof, 0) >= 0)
+				i = eof;	/* Search for further files starting from the end of this one */
+
+		} else {
+			if (eop < 0)
+				i = (-eop);
+		}
+	}
+}
+
+int paldevel_describe (int row)
+{
+	int i, s;
+	int hd[HEADERSIZE];
+	int en, tp, sp, lp;
+
+	int b, rd_err;
+
+
+	en = ft[THISLOADER].en;
+	tp = ft[THISLOADER].tp;
+	sp = ft[THISLOADER].sp;
+	lp = ft[THISLOADER].lp;
+
+	/* Note: addblockdef() is the glue between ft[] and blk[], so we can now read from blk[] */
+	s = blk[row] -> p2;
+
+	/* Read header (it's safe to read it here for it was already decoded during the search stage) */
+	for (i = 0; i < HEADERSIZE; i++)
+		hd[i]= readttbyte(s + i * BITSINABYTE, lp, sp, tp, en);
+
+	/* Extract load and end locations */
+	blk[row]->cs = hd[LOADOFFSETL] + (hd[LOADOFFSETH] << 8);
+	blk[row]->ce = hd[ENDOFFSETL]  + (hd[ENDOFFSETH]  << 8);
+
+	/* Prevent int wraparound when subtracting 1 from end location
+	   to get the location of the last loaded byte */
+	if (blk[row]->ce == 0)
+		blk[row]->ce = 0xFFFF;
+	else
+		(blk[row]->ce)--;
+
+	/* Compute size */
+	blk[row]->cx = blk[row]->ce - blk[row]->cs + 1;
+
+	/* Compute pilot & trailer lengths */
+
+	/* pilot is in bytes... */
+	blk[row]->pilot_len = (blk[row]->p2 - blk[row]->p1) / BITSINABYTE;
+
+	/* ... trailer in pulses */
+	blk[row]->trail_len = blk[row]->p4 - blk[row]->p3 - (BITSINABYTE - 1);
+
+	/* if there IS pilot then disclude the sync byte */
+	if (blk[row]->pilot_len > 0)
+		blk[row]->pilot_len -= SYNCSEQSIZE;
+
+	/* Extract data */
+	rd_err = 0;
+
+	s = blk[row]->p2 + (HEADERSIZE * BITSINABYTE);
+
+	if (blk[row]->dd != NULL)
+		free(blk[row]->dd);
+
+	blk[row]->dd = (unsigned char*)malloc(blk[row]->cx);
+
+	for (i = 0; i < blk[row]->cx; i++) {
+		b = readttbyte(s + (i * BITSINABYTE), lp, sp, tp, en);
+		if (b != -1) {
+			blk[row]->dd[i] = b;
+		} else {
+			blk[row]->dd[i] = 0x69;  /* error code */
+			rd_err++;
+
+			/* for experts only */
+			sprintf(lin, "\n - Read Error on byte @$%X (prg data offset: $%04X)", s + (i * BITSINABYTE), i + 2);
+			strcat(info, lin);
+		}
+	}
+
+	blk[row]->rd_err = rd_err;
+
+	return(rd_err);
+}
