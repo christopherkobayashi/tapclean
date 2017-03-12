f965042f22f9d0b4be7cd325e8f16f968a58ab45
Luigi Di Fraia <luigidifraia@users.sourceforge.net>
2017-03-12 23:25:08 +0000

    Updated version and copyright date

--- src/mydefs.h
+++ src/mydefs.h
@@ -39,8 +39,8 @@
 #define SLASH   '/'
 #endif
 
-#define VERSION_STR "0.33-pre-13"
-#define COPYRIGHT_STR "(C) 2006-2016 TC Team"
+#define VERSION_STR "0.33"
+#define COPYRIGHT_STR "(C) 2006-2017 TC Team"
 #define BUILDER_STR     "ldf"
 
 #define TRUE	1
