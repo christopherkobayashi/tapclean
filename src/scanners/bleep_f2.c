2ea4c9bbe75609b085f18cc2b1aad43a346e6fb2
Luigi Di Fraia <luigidifraia@users.sourceforge.net>
2017-11-13 15:22:37 +0000

    Rewritten the scanner for Bleepload Special

--- src/scanners/bleep_f2.c
+++ src/scanners/bleep_f2.c
@@ -1,226 +1,326 @@
-/*---------------------------------------------------------------------------
-  bleep_f2.c (Bleepload Special)
-  
-  Part of project "Final TAP". 
-  
-  A Commodore 64 tape remastering and data extraction utility.
+/*
+ * bleep_f2.c (rewritten by Luigi Di Fraia, Nov 2017)
+ *
+ * Part of project "TAPClean". May be used in conjunction with "Final TAP".
+ *
+ * Final TAP is (C) 2001-2006 Stewart Wilson, Subchrist Software.
+ *
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+ * St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
 
-  (C) 2001-2006 Stewart Wilson, Subchrist Software.
-   
-  
-   
-   This program is free software; you can redistribute it and/or modify it under 
-   the terms of the GNU General Public License as published by the Free Software 
-   Foundation; either version 2 of the License, or (at your option) any later 
-   version.
-   
-   This program is distributed in the hope that it will be useful, but WITHOUT ANY 
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-   PARTICULAR PURPOSE. See the GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License along with 
-   this program; if not, write to the Free Software Foundation, Inc., 51 Franklin 
-   St, Fifth Floor, Boston, MA 02110-1301 USA
-
-
-   Notes:-
-
-   Pilot byte values vary across tapes, therefore this code has to search for
-   ALL possible values when locating blocks.
-
----------------------------------------------------------------------------*/
+/*
+ * Status: Beta
+ *
+ * CBM inspection needed: No
+ * Single on tape: No
+ * Sync: Sequence (bytes)
+ * Header: Yes
+ * Data: Continuous
+ * Checksum: Yes
+ * Post-data: No
+ * Trailer: No
+ * Trailer homogeneous: N/A
+ */
 
 #include "../mydefs.h"
 #include "../main.h"
 
 #include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/*---------------------------------------------------------------------------*/
+#define THISLOADER	BLEEP_SPC
+
+#define BITSINABYTE	8	/* a byte is made up of 8 bits here */
+
+#define SYNCSEQSIZE	2	/* amount of sync bytes */
+#define MAXTRAILER	2040	/* max amount of trailer pulses read in */
+
+#define HEADERSIZE	7	/* size of block header */
+
+#define BLKNUMOFFSET	0	/* block number offset inside header */
+#define LOADOFFSETL	1	/* load location (LSB) offset inside header */
+#define LOADOFFSETH	2	/* load location (MSB) offset inside header */
+#define ENDOFFSETL	3	/* end  location (LSB) offset inside header */
+#define ENDOFFSETH	4	/* end  location (MSB) offset inside header */
+#define EXECOFFSETL	5	/* execution address (LSB) offset inside header */
+#define EXECOFFSETH	6	/* execution address (MSB) offset inside header */
+
+int bleep_spc_search_core(int first_sof, int pv)
+{
+	int i, h;			/* counters */
+	int sof, sod, eod, eof, eop;	/* file offsets */
+	int last_eof;
+	int hd[HEADERSIZE];		/* buffer to store block header info */
+
+	int en, tp, sp, lp;		/* encoding parameters */
+
+	unsigned int s, e;		/* block locations referred to C64 memory */
+	unsigned int x; 		/* block size */
+
+	int sypat[SYNCSEQSIZE];		/* expected sync pattern */
+
+
+	en = ft[THISLOADER].en;
+	tp = ft[THISLOADER].tp;
+	sp = ft[THISLOADER].sp;
+	lp = ft[THISLOADER].lp;
+
+	sypat[0] = pv ^ 0xFF;
+	sypat[1] = pv;
+
+	for (last_eof = first_sof, i = first_sof; i > 0 && i < tap.len - BITSINABYTE; i++) {
+		ft[THISLOADER].pv = pv;
+		ft[THISLOADER].sv = pv ^ 0xFF;
+		ft[THISLOADER].pmin = 5;
+		ft[THISLOADER].pmax = NA;
+
+		eop = find_pilot(i, THISLOADER);
+
+		if (eop > 0) {
+			/* Valid pilot found, mark start of file */
+			sof = i;
+			i = eop;
+
+			/* Decode a SYNCSEQSIZE byte sequence (possibly a valid sync train) */
+			for (h = 0; h < SYNCSEQSIZE; h++) {
+				if (readttbyte(i + (h * BITSINABYTE), lp, sp, tp, en) != sypat[h])
+					break;
+			}
+
+			/* Sync train doesn't match */
+			if (h != SYNCSEQSIZE)
+				continue;
+
+			/* Valid sync train found, mark start of data */
+			sod = i + SYNCSEQSIZE * BITSINABYTE;
+
+			/* Read header */
+			for (h = 0; h < HEADERSIZE; h++) {
+				hd[h] = readttbyte(sod + h * BITSINABYTE, lp, sp, tp, en);
+				if (hd[h] == -1)
+					break;
+			}
+
+			/* Bail out if there was an error reading the block header */
+			if (h != HEADERSIZE)
+				continue;
+
+			/* Extract block load and chain end locations */
+			s = hd[LOADOFFSETL] + (hd[LOADOFFSETH] << 8);
+			e = hd[ENDOFFSETL]  + (hd[ENDOFFSETH]  << 8);
+
+			if (e < s)
+				continue;
+
+			/* Set size */
+			x = 0x100;
+
+			/* Point to the first pulse of the checkbyte (that's final) */
+			eod = sod + (HEADERSIZE + x) * BITSINABYTE;
+
+			/* Initially point to the last pulse of the checkbyte */
+			eof = eod + BITSINABYTE - 1;
+
+			/* Include pilot tone if this is the first block in a chain */
+			if (hd[BLKNUMOFFSET] == 0)
+				sof = first_sof;
+
+			/* Include trailer if this is the last block */
+			if (e - s == 0x100) {
+				/* Trace 'eof' to end of trailer (bit 1 pulses only) */
+				h = 0;
+				while (eof < tap.len - 1 &&
+						h++ < MAXTRAILER &&
+						readttbit(eof + 1, lp, sp, tp) == 1)
+					eof++;
+			}
+
+			if (addblockdef(THISLOADER, sof, sod, eod, eof, pv) >= 0) {
+				i = eof;	/* Search for further files starting from the end of this one */
+				last_eof = eof;
+			}
+
+		} else {
+			if (eop < 0)	/* find_pilot failed (too few/many), set i to failure point. */
+				i = (-eop);
+		}
+	}
+
+	return last_eof;
+}
+
 void bleep_spc_search(void)
 {
-   int cnt,cnt2,i,sof,sod,eod,eof;
-   int k,pi=0,start,zcnt,len;
-   int plt,byt,tmp, dr=0;
-   unsigned char done;
-   unsigned char p[100]; /* holds a list of "bleep special" pb's found in the tap */
+	int i;				/* counter */
+	int sof, eop;			/* file offsets */
 
-   if(!quiet)
-      msgout("  Bleepload Special");
-         
-
-   plt=0x0;  /* set inital pilot value. */
-   /* (park patrol=$2A, thrust=$50, iball=$47)  */
-
-   
-   /* search for a valid pilot sequence using ANY pb value...  */
-   for(cnt=0; cnt<tap.len-8; cnt++)
-   {
-      byt=readttbyte(cnt, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF);
-
-      if(byt!=-1 && byt!=0x00 && byt!=0xFF)
-      {
-         i=0;
-         cnt2=cnt; /* preserve original counter.  */
-         do
-         {
-            tmp=readttbyte(cnt2, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF);
-            cnt2+=8;
-            i++;
-         }
-         while(tmp==byt);
-         cnt2-=8;
-
-         if(i==10)  /* found pb*10...  */
-         {
-            if(readttbyte(cnt2, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF)==(byt^0xFF))  /* if pb XOR $FF is correct...  */
-            {
-               if(readttbyte(cnt2+8, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF)==byt)  /* if last pb is correct...  */
-               {
-                  if(readttbyte(cnt2+16, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF)==0)  /* if first byte is 0... */
-                  {
-                     plt= byt;   /* FOUND correct bleep special pilot for this tap!. */
-                  }
-               }
-            }
-         }
-      }
-      if(plt!=0)
-      {
-         p[pi++]=plt;  /* record this "bleepload special" pb.  */
-         plt=0;
-         cnt=cnt2;
-      }
-   }
-
-   if(pi==0)
-      p[pi++]=0x47;  /* insert a default pb if we didnt find any */
+	int en, tp, sp, lp, pv, sv;	/* encoding parameters */
 
 
-   /* now scan the tap for bleepload special chains using each of the pilot
-   values we discovered above...   */
+	en = ft[THISLOADER].en;
+	tp = ft[THISLOADER].tp;
+	sp = ft[THISLOADER].sp;
+	lp = ft[THISLOADER].lp;
 
+	if (!quiet)
+		msgout("  Bleepload Special");
 
-   k=0;
-   do
-   {
-      plt= p[k];   /* guaranteed to be at least 1 pb value available.  */
+	for (i = 20; i > 0 && i < tap.len - BITSINABYTE; i++) {
+		ft[THISLOADER].pv = 1;
+		ft[THISLOADER].sv = 0;
+		ft[THISLOADER].pmin = 1024;
+		ft[THISLOADER].pmax = NA;
 
-      for(cnt=20; cnt<tap.len-8 && dr!=DBFULL; cnt++)
-      {
-         byt=readttbyte(cnt, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF);
-         if(byt==plt)
-         {
-            start = cnt;
-            /* trace "start" back to start of L pulse leader... */
-            if(tap.tmem[start-1]>ft[BLEEP].lp-tol && tap.tmem[start-1]<ft[BLEEP].lp+tol)
-            {
-               while(tap.tmem[start-1]>ft[BLEEP].lp-tol && tap.tmem[start-1]<ft[BLEEP].lp+tol && !is_pause_param(start-1))
-                  start--;
-            }
+		eop = find_pilot(i, THISLOADER);
 
-            zcnt=0;
-            while(readttbyte(cnt, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF)==plt)
-            {
-               cnt+=8;
-               zcnt++;
-            }
-            if(zcnt>8 && readttbyte(cnt, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF)==(plt^0xFF))  /* ending with a sync.  */
-            {
-               if(readttbyte(cnt+8, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF)==plt) /* after sync is one more pilot */
-               {
-                  cnt+=16;
+		if (eop > 0) {
+			/* Valid pilot found, mark start of file */
+			sof = i;
+			i = eop;
 
-                  tmp=0; /* look for block number 0 initially  */
-                  do
-                  {
-                     if(readttbyte(cnt, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF)==tmp) /* block num is correct. */
-                     {
-                        if(tmp==0)
-                           sof = start;  /* use start of leader if 1st block.  */
-                        else
-                           sof= cnt-(12*8);
-                        sod = cnt;
-                        len = 256+8;  /* +8 accounts for checkbyte  */
-                        eod = sod+(len*8)-8;
-                        eof = eod+7;
+			pv = readttbyte(i, lp, sp, tp, en);
 
-                        /* override 'eof' if there is a trailer tone...  */
-                        if(readttbyte(eof+1, ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF)==0xFF)
-                        {
-                           while(eof<tap.len-1 && tap.tmem[eof+1]>ft[BLEEP].lp-tol && tap.tmem[eof+1]<ft[BLEEP].lp+tol)
-                              eof++;
-                        }
+			if (pv <= 0)
+				continue;
 
-                        dr =addblockdef(BLEEP_SPC, sof,sod,eod,eof, plt);
-                        tmp++; /* bump to look for next block number..  */
-                        cnt+=2208;
-                        done=0;
-                     }
-                     else
-                        done=1;  /* failed to find consecutive block number. */
-                  }
-                  while(!done && dr!=DBFULL);
-               }
-            }
-         }
-      }
-      k++;
-   }
-   while(k<pi && dr!=DBFULL);
+			do {
+				i += BITSINABYTE;
+			} while (readttbyte(i, lp, sp, tp, en) == pv);
+				
+
+			sv = readttbyte(i, lp, sp, tp, en);
+
+			if (sv < 0 || sv != (pv ^ 0xFF))
+				continue;
+
+			i += BITSINABYTE;
+
+			if (readttbyte(i, lp, sp, tp, en) != pv)
+				continue;
+
+			i += BITSINABYTE;
+
+			if (readttbyte(i, lp, sp, tp, en) != 0x00)
+				continue;
+
+			i = bleep_spc_search_core(sof, pv);
+
+		} else {
+			if (eop < 0)	/* find_pilot failed (too few/many), set i to failure point. */
+				i = (-eop);
+		}
+	}
 }
-/*---------------------------------------------------------------------------*/
+
 int bleep_spc_describe(int row)
 {
-   int s,i;
-   int hd[8],b,rd_err,cb;
+	int i, s;
+	int hd[HEADERSIZE];
+	int en, tp, sp, lp;
+	int cb;
 
-   sprintf(lin,"\n - Pilot byte : $%02X", blk[row]->xi);
-   strcat(info,lin);
+	int b, rd_err;
 
-   s= blk[row]->p2;  /* get first data offset   */
 
-   for(i=0; i<8; i++)
-      hd[i]= readttbyte(s+(i*8), ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF);
+	en = ft[blk[row]->lt].en;
+	tp = ft[blk[row]->lt].tp;
+	sp = ft[blk[row]->lt].sp;
+	lp = ft[blk[row]->lt].lp;
 
-   sprintf(lin,"\n - Block Number : $%02X", hd[0]);
-   strcat(info,lin);
+	/* Note: addblockdef() is the glue between ft[] and blk[], so we can now read from blk[] */
+	s = blk[row]->p2;
 
-   blk[row]->cs= hd[1]+(hd[2]<<8);
-   blk[row]->cx= 256;
-   blk[row]->ce= ((blk[row]->cs + blk[row]->cx)-1);
+	/* Read header (it's safe to read it here for it was already decoded during the search stage) */
+	for (i = 0; i < HEADERSIZE; i++)
+		hd[i] = readttbyte(s + i * BITSINABYTE, lp, sp, tp, en);
 
-   /* get execution address...   */
-   sprintf(lin,"\n - Exe Address : $%04X", hd[5]+(hd[6]<<8));
-   strcat(info,lin);
+	/* Extract load and end locations */
+	blk[row]->cs = hd[LOADOFFSETL] + (hd[LOADOFFSETH] << 8);
+	blk[row]->ce = blk[row]->cs + 0x00FF;
 
-   /* get pilot length... */
-   blk[row]->pilot_len = ((blk[row]->p2 - blk[row]->p1)/8) -2;
-   blk[row]->trail_len =0;
+	/* Set size */
+	blk[row]->cx = 0x100;
 
-   /* extract data and test checksum...  */
-   rd_err=0;
-   cb=0;
-   s= (blk[row]->p2)+(7*8);
+	/* Compute pilot & trailer lengths */
 
-   if(blk[row]->dd!=NULL)
-      free(blk[row]->dd);
-   blk[row]->dd= (unsigned char*)malloc(blk[row]->cx);
+	/* pilot is in bytes... */
+	blk[row]->pilot_len = (blk[row]->p2 - blk[row]->p1) / BITSINABYTE;
 
-   for(i=0; i<blk[row]->cx; i++)
-   {
-      b= readttbyte(s+(i*8), ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF);
-      cb^=b;
-      if(b==-1)
-         rd_err++;
-      blk[row]->dd[i]= b;
-   }
-   b= readttbyte(s+(i*8), ft[BLEEP].lp, ft[BLEEP].sp, ft[BLEEP].tp, MSbF); /* read checkbyte  */
+	/* ... trailer in pulses */
+	blk[row]->trail_len = blk[row]->p4 - blk[row]->p3 - (BITSINABYTE - 1);
 
-   blk[row]->cs_exp= cb &0xFF;
-   blk[row]->cs_act= b;
-   blk[row]->rd_err= rd_err;
-   return 0;
+	/* if there IS pilot then disclude the sync sequence */
+	if (blk[row]->pilot_len > 0)
+		blk[row]->pilot_len -= SYNCSEQSIZE;
+
+	/* Print pilot byte */
+	sprintf(lin, "\n - Pilot byte : $%02X", blk[row]->xi);
+	strcat(info, lin);
+
+	/* Extract block ID, chain end and execution address and print these out */
+	sprintf(lin, "\n - Block Number : $%02X", hd[BLKNUMOFFSET]);
+	strcat(info, lin);
+	sprintf(lin, "\n - Chain end address : $%04X", hd[ENDOFFSETL] + (hd[ENDOFFSETH] << 8) - 1);
+	strcat(info, lin);
+	sprintf(lin, "\n - Exe Address : $%04X", hd[EXECOFFSETL] + (hd[EXECOFFSETH] << 8));
+	strcat(info, lin);
+
+	/* Extract data and test checksum... */
+	rd_err = 0;
+	cb = 0;
+
+	s = blk[row]->p2 + (HEADERSIZE * BITSINABYTE);
+
+	if (blk[row]->dd != NULL)
+		free(blk[row]->dd);
+
+	blk[row]->dd = (unsigned char*)malloc(blk[row]->cx);
+
+	for (i = 0; i < blk[row]->cx; i++) {
+		b = readttbyte(s + (i * BITSINABYTE), lp, sp, tp, en);
+		cb ^= b;
+
+		if (b != -1) {
+			blk[row]->dd[i] = b;
+		} else {
+			blk[row]->dd[i] = 0x69;  /* error code */
+			rd_err++;
+
+			/* for experts only */
+			sprintf(lin, "\n - Read Error on byte @$%X (prg data offset: $%04X)", s + (i * BITSINABYTE), i + 2);
+			strcat(info, lin);
+		}
+	}
+
+	b = readttbyte(s + (i * BITSINABYTE), lp, sp, tp, en);
+	if (b == -1) {
+		/* Even if not within data, we cannot validate data reliably if
+		   checksum is unreadable, so that increase read errors */
+		rd_err++;
+
+		/* for experts only */
+		sprintf(lin, "\n - Read Error on checkbyte @$%X", s + (i * BITSINABYTE));
+		strcat(info, lin);
+	}
+
+	blk[row]->cs_exp = cb & 0xFF;
+	blk[row]->cs_act = b  & 0xFF;
+	blk[row]->rd_err = rd_err;
+
+	return(rd_err);
 }
-
