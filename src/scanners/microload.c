d21deda4af6ce3bb7602d917f1aed0e2e48b93c3
Luigi Di Fraia <luigidifraia@gmail.com>
2018-10-01 23:49:14 +0100

    Rewritten

--- src/scanners/microload.c
+++ src/scanners/microload.c
@@ -1,136 +1,230 @@
 /*
- * microload.c
+ * microload.c (rewritten by Luigi Di Fraia, Oct 2018)
  *
- * Part of project "Final TAP". 
+ * Part of project "TAPClean". May be used in conjunction with "Final TAP".
  *
- * A Commodore 64 tape remastering and data extraction utility.
- *
- * (C) 2001-2006 Stewart Wilson, Subchrist Software.
+ * Final TAP is (C) 2001-2006 Stewart Wilson, Subchrist Software.
  *
  *
  *
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
- * Foundation; either version 2 of the License, or (at your option) any later 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
  * version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with 
- * this program; if not, write to the Free Software Foundation, Inc., 51 Franklin 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
  * St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  */
 
+/*
+ * Status: Beta
+ *
+ * CBM inspection needed: No
+ * Single on tape: No
+ * Pilot: Byte (0xA0, 256 of them)
+ * Sync: Sequence (bytes: 0x0A down to 0x01)
+ * Header: Yes
+ * Data: Continuous
+ * Checksum: Yes
+ * Post-data: No
+ * Trailer: No
+ * Trailer homogeneous: N/A
+ */
 
 #include "../mydefs.h"
 #include "../main.h"
 
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#define HDSZ 4
+#define THISLOADER	MICROLOAD
 
+#define BITSINABYTE	8	/* a byte is made up of 8 bits here */
+
+#define SYNCSEQSIZE	10	/* amount of sync bytes */
+#define MAXTRAILER	8	/* max amount of trailer pulses read in */
+
+#define HEADERSIZE	4	/* size of block header */
+
+#define LOADOFFSETH	1	/* load location (MSB) offset inside header */
+#define LOADOFFSETL	0	/* load location (LSB) offset inside header */
+#define DATAOFFSETH	3	/* two's complement of data size (MSB) offset inside header */
+#define DATAOFFSETL	2	/* two's complement of data size (LSB) offset inside header */
 
 void micro_search(void)
 {
-	int i, sof, sod, eod, eof;
-	int en, tp, sp, lp, sv;
-	int x, z, h, hd[HDSZ];
+	int i, h;			/* counters */
+	int sof, sod, eod, eof, eop;	/* file offsets */
+	int hd[HEADERSIZE];		/* buffer to store block header info */
 
-	en = ft[MICROLOAD].en;	/* set endian according to table in main.c */
-	tp = ft[MICROLOAD].tp;	/* set threshold */
-	sp = ft[MICROLOAD].sp;	/* set short pulse */
-	lp = ft[MICROLOAD].lp;	/* set long pulse */
-	sv = ft[MICROLOAD].sv;	/* set sync value */
+	int en, tp, sp, lp, sv;		/* encoding parameters */
+
+	unsigned int s, e;		/* block locations referred to C64 memory */
+	unsigned int x; 		/* block size */
+
+
+	en = ft[THISLOADER].en;
+	tp = ft[THISLOADER].tp;
+	sp = ft[THISLOADER].sp;
+	lp = ft[THISLOADER].lp;
+	sv = ft[THISLOADER].sv;
 
 	if (!quiet)
 		msgout("  Microload");
-         
-	for (i = 20; i < tap.len - 8; i++) {
-		if ((z = find_pilot(i, MICROLOAD)) > 0) {
+
+	for (i = 20; i > 0 && i < tap.len - BITSINABYTE; i++) {
+		eop = find_pilot(i, THISLOADER);
+
+		if (eop > 0) {
+			/* Valid pilot found, mark start of file */
 			sof = i;
-			i = z;
-			if (readttbyte(i, lp, sp, tp, en) == sv) {
+			i = eop;
 
-				/* i should really check whole sequence */
+			/* Check if there's a valid sync byte for this loader */
+			if (readttbyte(i, lp, sp, tp, en) != sv)
+				continue;
 
-				if (readttbyte(i + 8, lp, sp, tp, en) == 0x09) {
-					sod = i + (10 * 8);
+			/* Valid sync found, mark start of data */
+			sod = i + SYNCSEQSIZE * BITSINABYTE;
 
-					/* decode the header... */
-
-					for (h = 0; h < HDSZ; h++) {
-						hd[h] = readttbyte(sod + (h * 8), lp, sp, tp, en);
-						if (hd[h] == -1)
-							break;
-					}
-					if (h != HDSZ)
-						continue;
-
-					x = ((hd[2] + (hd[3] << 8)) ^ 0xFFFF) + 1;	/* get length */
-               
-					eod = sod + (x * 8) + (HDSZ * 8);
-					eof = eod + 7;
-
-					addblockdef(MICROLOAD, sof, sod, eod, eof, 0);
-					i = eof;	/* optimize search */
-				}
+			/* Read header */
+			for (h = 0; h < HEADERSIZE; h++) {
+				hd[h] = readttbyte(sod + h * BITSINABYTE, lp, sp, tp, en);
+				if (hd[h] == -1)
+					break;
 			}
+
+			/* Bail out if there was an error reading the block header */
+			if (h != HEADERSIZE)
+				continue;
+
+			/* Extract load location and size */
+			s = hd[LOADOFFSETL] + (hd[LOADOFFSETH] << 8);
+			x = 0x10000 - (hd[DATAOFFSETL] + (hd[DATAOFFSETH] << 8));
+
+			/* Compute C64 memory location of the _LAST loaded byte_ */
+			e = s + x - 1;
+
+			/* Plausibility check */
+			if (e > 0xFFFF)
+				continue;
+
+			/* Point to the first pulse of the checkbyte (that's final) */
+			eod = sod + (HEADERSIZE + x) * BITSINABYTE;
+
+			/* Initially point to the last pulse of the checkbyte */
+			eof = eod + BITSINABYTE - 1;
+
+			/* Trace 'eof' to end of trailer (any value, both bit 1 and bit 0 pulses) */
+			/* Note: No trailer has been documented, but we are not strictly
+				 requiring one here, just checking for it is future-proof */
+			h = 0;
+			while (eof < tap.len - 1 &&
+					h++ < MAXTRAILER &&
+					readttbit(eof + 1, lp, sp, tp) >= 0)
+				eof++;
+
+			if (addblockdef(THISLOADER, sof, sod, eod, eof, 0) >= 0)
+				i = eof;	/* Search for further files starting from the end of this one */
+
+		} else {
+			if (eop < 0)	/* find_pilot failed (too few/many), set i to failure point. */
+				i = (-eop);
 		}
 	}
 }
 
 int micro_describe(int row)
 {
-	int i, s, b, rd_err, hd[HDSZ], cb;
+	int i, s;
+	int hd[HEADERSIZE];
 	int en, tp, sp, lp;
+	int cb;
 
-	en = ft[MICROLOAD].en;	/* set endian according to table in main.c */
-	tp = ft[MICROLOAD].tp;	/* set threshold */
-	sp = ft[MICROLOAD].sp;	/* set short pulse */
-	lp = ft[MICROLOAD].lp;	/* set long pulse */
+	int b, rd_err;
 
-	/* decode the header... */
 
+	en = ft[THISLOADER].en;
+	tp = ft[THISLOADER].tp;
+	sp = ft[THISLOADER].sp;
+	lp = ft[THISLOADER].lp;
+
+	/* Set read pointer to the beginning of the payload */
 	s = blk[row]->p2;
-	for (i = 0; i < HDSZ; i++)
-	hd[i] = readttbyte(s + (i * 8), lp, sp, tp, en);
 
-	/* compute C64 start address, end address and size... */
+	/* Read header (it's safe to read it here for it was already decoded during the search stage) */
+	for (i = 0; i < HEADERSIZE; i++)
+		hd[i] = readttbyte(s + i * BITSINABYTE, lp, sp, tp, en);
 
-	blk[row]->cs = hd[0] + (hd[1] << 8);
-	blk[row]->cx = ((hd[2] + (hd[3] << 8)) ^ 0xFFFF) + 1;
-	blk[row]->ce = (blk[row]->cs + blk[row]->cx) - 1;
+	/* Read/compute C64 memory location for load/end address, and read data size */
+	blk[row]->cs = hd[LOADOFFSETL] + (hd[LOADOFFSETH] << 8);
+	blk[row]->cx = 0x10000 - (hd[DATAOFFSETL] + (hd[DATAOFFSETH] << 8));
 
-	/* get pilot & trailer lengths */
+	/* C64 memory location of the _LAST loaded byte_ */
+	blk[row]->ce = blk[row]->cs + blk[row]->cx - 1;
 
-	blk[row]->pilot_len = ((blk[row]->p2 - blk[row]->p1) - (10 * 8)) >> 3;
-	blk[row]->trail_len = (blk[row]->p4 - blk[row]->p3 - 7) >> 3;
+	/* Compute pilot & trailer lengths */
 
-	/* extract data and test checksum... */
+	/* pilot is in bytes... */
+	blk[row]->pilot_len = (blk[row]->p2 - blk[row]->p1) / BITSINABYTE;
 
+	/* ... trailer in pulses */
+	/* Note: No trailer has been documented, but we are not pretending it
+	         here, just checking for it is future-proof */
+	blk[row]->trail_len = blk[row]->p4 - blk[row]->p3 - (BITSINABYTE - 1);
+
+	/* if there IS pilot then disclude the sync sequence */
+	if (blk[row]->pilot_len > 0)
+		blk[row]->pilot_len -= SYNCSEQSIZE;
+
+	/* Extract data and test checksum... */
 	rd_err = 0;
 	cb = 0;
-	s = (blk[row]->p2) + (HDSZ * 8);
+
+	s = blk[row]->p2 + (HEADERSIZE * BITSINABYTE);
 
 	if (blk[row]->dd != NULL)
 		free(blk[row]->dd);
+
 	blk[row]->dd = (unsigned char*)malloc(blk[row]->cx);
-      
+
 	for (i = 0; i < blk[row]->cx; i++) {
-		b = readttbyte(s + (i * 8), lp, sp, tp, en);
+		b = readttbyte(s + (i * BITSINABYTE), lp, sp, tp, en);
 		cb ^= b;
-		if (b == -1)
-		rd_err++;
-		blk[row]->dd[i] = b;
+
+		if (b != -1) {
+			blk[row]->dd[i] = b;
+		} else {
+			blk[row]->dd[i] = 0x69;	/* sentinel error value */
+			rd_err++;
+
+			/* for experts only */
+			sprintf(lin, "\n - Read Error on byte @$%X (prg data offset: $%04X)", s + (i * BITSINABYTE), i + 2);
+			strcat(info, lin);
+		}
 	}
-	b = readttbyte(s + (i * 8), lp, sp, tp, en);
+
+	b = readttbyte(s + (i * BITSINABYTE), lp, sp, tp, en);
+	if (b == -1) {
+		/* Even if not within data, we cannot validate data reliably if
+		   checksum is unreadable, so that increase read errors */
+		rd_err++;
+
+		/* for experts only */
+		sprintf(lin, "\n - Read Error on checkbyte @$%X", s + (i * BITSINABYTE));
+		strcat(info, lin);
+	}
+
 	blk[row]->cs_exp = cb & 0xFF;
-	blk[row]->cs_act = b;
+	blk[row]->cs_act = b  & 0xFF;
 	blk[row]->rd_err = rd_err;
 
-	return 0;
+	return(rd_err);
 }
-
