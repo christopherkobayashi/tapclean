a9853d6ad6f33aee1302b2894e6a8886cd0b4933
Luigi Di Fraia <luigidifraia@gmail.com>
2019-01-08 13:24:48 +0000

    Bumped version and copyright end date

--- src/mydefs.h
+++ src/mydefs.h
@@ -39,8 +39,8 @@
 #define SLASH	'/'
 #endif
 
-#define VERSION_STR	"0.38-pre-17"
-#define COPYRIGHT_STR	"(C)2006-2018 TC Team"
+#define VERSION_STR	"0.38"
+#define COPYRIGHT_STR	"(C)2006-2019 TC Team"
 #define BUILDER_STR	"ldf"
 
 #define TRUE	1
